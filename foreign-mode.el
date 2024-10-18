77d2d2c28752a25d77321c44ed1f581d0ea8ea6d
Prikaz98 <77636647+Prikaz98@users.noreply.github.com>
2024-10-18 18:04:32 +0200

    Update foreign-mode.el

--- foreign-mode.el
+++ foreign-mode.el
@@ -9,7 +9,7 @@
 ;; Version: 0.0.1
 ;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
 ;; Homepage: https://github.com/ivan/foreign-mode
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((emacs "29.3"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -24,12 +24,13 @@
 (defun foreign-mode-next-item ()
   "Go to editing next item."
   (interactive)
-  (re-search-forward "^.+ \\w+ - \\(\\w+\\)?$"))
+  (forward-line 1)
+  (end-of-line))
 
 (defun foreign-mode-prev-item ()
   "Go to editing prev item."
   (interactive)
-  (re-search-backward "^.+ \\w+ - \\(\\w+\\)?$")
+  (forward-line -1)
   (end-of-line))
 
 (defvar foreign-mode-map
